f6a4f8f9cc5a3aa4c64f8c51a6c4c25d79b32c1f
dana nur fiqi <danufiq98@gmail.com>
2023-02-06 18:29:48 +0700

    add app main

---
+++ app/src/main/java/com/example/banksephora/main/screen/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.banksephora.main.screen.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.banksephora.R
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
